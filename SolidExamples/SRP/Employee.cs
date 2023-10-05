ee62e454413e7330dd7e15ad957bd7f4fddd17cf
Andrea Bignardi <a.bignardi@dotblue.it>
2023-10-05 08:39:22 +0200

    SRP - start

---
+++ SolidExamples/SRP/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolidExamples.SRP
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Wage { get; set; }
+
+        public Employee(int id, string name, double wage)
+        {
+            Id = id;
+            Name = name;
+            Wage = wage;
+        }
+    }
+}
---
+++ SolidExamples/SRP/EmployeeManager.cs
@@ -0,0 +1,34 @@
+﻿namespace SolidExamples.SRP
+{
+    public class EmployeeManager
+    {
+
+        /*
+         * SRP: Single Responsibility Principle
+         *  "A class should have only one reason to change"
+         *  Accentrando più di una responsabilità in un'unica classe, si crea rigidità e "accoppiamento" (coupling) di un comportamento con un altro, e il cambio di
+         *  requisiti di uno dei comportamenti potrebbe sfociare in una modifica alla stessa classe e possibilmente anche a porzioni di codice che non interessano
+         *  necessariamente all'ambito della modifica. 
+         *  In breve: più i requisiti richiano di cambiare su una delle responsabilità della classe, più si rende necessario disaccoppiarle
+         */
+
+        public double CalculatePayroll(int id, double hours)
+        {
+            var employee = GetEmployee(id);
+
+            return employee.Wage * hours;
+        }
+
+        public void PersistToDb(Employee employee)
+        {
+            //WRITE ON DB
+        }
+
+        public Employee GetEmployee(int id)
+        {
+            return new Employee(id, "Tizio", 30);
+        }
+
+
+    }
+}
