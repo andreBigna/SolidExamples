eefbe2b581f3db8d7153d3a6a994a495324d2732
Andrea Bignardi <a.bignardi@dotblue.it>
2023-10-26 17:18:48 +0200

    Refactoring in folders

---
+++ SolidExamples/1 - SRP/1A - Broken SRP/EmployeeManager.cs
@@ -0,0 +1,41 @@
+﻿using SolidExamples.SRP;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolidExamples.BrokenSRP
+{
+
+    public class EmployeeManager
+    {
+
+        /*
+         * SRP: Single Responsibility Principle
+         *  "A class should have only one reason to change"
+         *  Accentrando più di una responsabilità in un'unica classe, si crea rigidità e "accoppiamento" (coupling) di un comportamento con un altro, e il cambio di
+         *  requisiti di uno dei comportamenti potrebbe sfociare in una modifica alla stessa classe e possibilmente anche a porzioni di codice che non interessano
+         *  necessariamente all'ambito della modifica. 
+         *  In breve: più i requisiti richiano di cambiare su una delle responsabilità della classe, più si rende necessario disaccoppiarle
+         */
+
+        public double CalculatePayroll(int id, double hours)
+        {
+            var employee = GetEmployee(id);
+
+            return employee.Wage * hours;
+        }
+
+        public void PersistToDb(Employee employee)
+        {
+            //WRITE ON DB
+        }
+
+        public Employee GetEmployee(int id)
+        {
+            return new Employee(id, "Tizio", 30);
+        }
+
+    }
+}
---
+++ SolidExamples/2 - OCP/2A - Broken OCP/ShapesDrawer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolidExamples.BrokenOCP
+{
+    public class ShapesDrawer
+    {
+        public void DrawShapes(IEnumerable<object> shapes)
+        {
+            foreach (object shape in shapes)
+            {
+                if (shape is Square square)
+                {
+                    DrawSquare(square);
+                }
+                else if (shape is Circle circle)
+                {
+                    DrawCircle(circle);
+                }
+            }
+        }
+
+        private void DrawSquare(Square square)
+        {
+            Console.WriteLine($"draws a {nameof(Square)}");
+        }
+
+        private void DrawCircle(Circle circle)
+        {
+            Console.WriteLine($"draws a {nameof(Circle)}");
+        }
+    }
+}
